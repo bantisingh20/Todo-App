7003a8294f8dd0fda5463efa74b00892baaa118e
bantisingh20 <singhbanti9900@gmail.com>
2024-09-09 15:22:10 +0530

    CRUD Todo

--- src/app/MyComponent/todos/todos.component.ts
+++ src/app/MyComponent/todos/todos.component.ts
@@ -1,26 +1,29 @@
-import { Component ,OnInit } from '@angular/core';
+import { Component ,OnInit ,ViewChild } from '@angular/core';
 import { CommonModule  } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {Todos} from './Todos'
 
+
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule ,FormsModule],
+  imports: [CommonModule ,FormsModule ],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit {
-  
+   
  Todolist = [
   {
     title:'Dinner'
     ,active : true
     ,status:'Close'
+     
   }
 
 ]
-
+   
+ 
 // use to load function after page load
 ngOnInit(): void {
   this.loadTodos();
@@ -33,13 +36,24 @@ loadTodos(): void {
 }
 
 newTodo: string = ''; 
+currentTodoIndex: number | null = null;
 
 addTodo() {
   if (this.newTodo.trim()) {
-    this.Todolist.push({ title: this.newTodo ,active :true ,status:'Close' });
-    this.newTodo = ''; 
+   
+    if(this.currentTodoIndex === null){
+      this.Todolist.push({ title: this.newTodo ,active :true ,status:'Close' });
+      this.newTodo = ''; 
+      localStorage.setItem('Todolist', JSON.stringify(this.Todolist));
 
-    localStorage.setItem('Todolist', JSON.stringify(this.Todolist));
+      
+    }
+    else{
+       
+      this.Todolist[this.currentTodoIndex].title = this.newTodo;
+      this.newTodo = ''; 
+      this.currentTodoIndex=null;
+    }
      
   }
 }
@@ -65,9 +79,16 @@ ChageStatusoftodo(index: number){
 deletethistodo(index: number){
   console.log(index);
   this.Todolist.splice(index, 1); 
+  localStorage.setItem('Todolist', JSON.stringify(this.Todolist));   
+}
 
-  localStorage.setItem('Todolist', JSON.stringify(this.Todolist));
-   
+
+
+updateTodo(index: number){
+  console.log(index); 
+  this.newTodo = this.Todolist[index].title;
+ //localStorage.setItem('Todolist', JSON.stringify(this.Todolist));
+  this.currentTodoIndex = index;
 }
 
 }
